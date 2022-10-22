11b41cfb5d4c9808abeb5c0ec43bcdabebc25a67
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-10-22 21:11:27 +0100

    construct word container

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,28 @@ const removeStartContainer = function () {
 
   startContainer.remove();
 };
+const renderBlankDivs = function (word) {
+  // convert word to array
+  const wordArray = word.split("");
+
+  // construct word-container div (parent)
+  const containerDiv = document.createElement("div");
+  containerDiv.setAttribute("id", "word-container");
+  containerDiv.setAttribute("class", "word-container");
+
+  // for each letter in word array create and append div to parent
+  wordArray.forEach(createAndAppendLetter);
+};
 
 //callback function to start game when button is clicked
 const startGame = function () {
   // remove the start container
   removeStartContainer();
+
+  const word = "hello";
+
   //render the blank divs
+  renderBlankDivs(word);
   //start timer
 };
 
