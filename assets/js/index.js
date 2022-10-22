a269bf280231ae63a8a90fd958651413037043f4
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-10-22 18:16:00 +0100

    remove start container

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
 const startBtn = document.getElementById("start-btn");
 
+// function to remove the start container
+const removeStartContainer = function () {
+  const startContainer = document.getElementById("start-container");
+
+  startContainer.remove();
+};
+
 //callback function to start game when button is clicked
 const startGame = function () {
-  console.log("clicked");
+  // remove the start container
+  removeStartContainer();
+  //render the blank divs
+  //start timer
 };
 
 // add start btn click event listener
